7fed0b129c48373b586eb4dabcca129bc8352503
daisy1754 <d-part@hotmail.co.jp>
2013-12-10 02:11:18 +0900

    intiial commit with auto-generated boilarplate

---
+++ demo/src/jp/gr/java_conf/daisy/chip_edit_text/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.daisy.chip_edit_text.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
